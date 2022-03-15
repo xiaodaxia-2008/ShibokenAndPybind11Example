a42c9b1c4d248fa8e00b1d0c7424e63a6cc1876d
xiaodaxia-2008 <xiaozisheng2008@qq.com>
2022-03-15 19:16:27 +0800

    refactor

--- Example.py
+++ Example.py
@@ -5,9 +5,15 @@ from build.PyEasyWidget import EasyWidget
 from IPython import embed
 
 app = QtWidgets.QApplication()
+win = QtWidgets.QMainWindow()
 w = EasyWidget()
 label = w.GetLabel()
 print(f"label text: {label.text()}")
 label.setText("Hello, change from python!")
-w.show()
+widget = w.Widget()
+layout = widget.layout()
+layout.addWidget(QtWidgets.QLabel("label from python!"))
+win.setCentralWidget(widget) # cause segmentation fault, it's maybe double destruction
+win.show()
+# w.show()
 app.exec_()
--- PyEasyWidget.cpp
+++ PyEasyWidget.cpp
@@ -4,24 +4,34 @@
 namespace py = pybind11;
 using namespace pybind11::literals;
 
+py::object WrapQtObject(void *instance, const char *qobject_name,
+                        bool is_borrow = true)
+{
+    PyObject *module = PyImport_ImportModule("shiboken2");
+    PyObject *dict = PyModule_GetDict(module);
+    PyObject *func = PyMapping_GetItemString(dict, "wrapInstance");
+    PyObject *args = PyTuple_New(2);
+    PyTuple_SetItem(args, 0, PyLong_FromVoidPtr(instance));
+    module = PyImport_ImportModule("PySide2.QtWidgets");
+    dict = PyModule_GetDict(module);
+    PyTuple_SetItem(args, 1, PyMapping_GetItemString(dict, qobject_name));
+    PyObject *obj = PyObject_CallObject(func, args);
+    py::handle h(obj);
+    return is_borrow ? py::reinterpret_borrow<py::object>(h)
+                     : py::reinterpret_steal<py::object>(h);
+}
+
 using namespace EW;
 PYBIND11_MODULE(PyEasyWidget, m)
 {
     py::class_<EasyWidget>(m, "EasyWidget")
         .def(py::init<>())
         .def("show", &EasyWidget::show)
-        .def("GetLabel", [](EasyWidget &self) {
-            PyObject *module = PyImport_ImportModule("shiboken2");
-            PyObject *dict = PyModule_GetDict(module);
-            PyObject *func = PyMapping_GetItemString(dict, "wrapInstance");
-            PyObject *args = PyTuple_New(2);
-            QLabel *label = self.GetLabel();
-            PyTuple_SetItem(args, 0, PyLong_FromVoidPtr((void *)label));
-            module = PyImport_ImportModule("PySide2.QtWidgets");
-            dict = PyModule_GetDict(module);
-            PyTuple_SetItem(args, 1, PyMapping_GetItemString(dict, "QLabel"));
-            PyObject *obj = PyObject_CallObject(func, args);
-            py::handle h(obj);
-            return py::reinterpret_borrow<py::object>(h);
+        .def("GetLabel",
+             [](EasyWidget &self) {
+                 return WrapQtObject(self.GetLabel(), "QLabel");
+             })
+        .def("Widget", [](EasyWidget &self) {
+            return WrapQtObject(&self, "QWidget", true);
         });
 }
